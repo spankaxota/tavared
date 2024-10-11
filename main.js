909dfc5a69e6aa48514a7d2af9cc580a43fa2e4a
tavares <140452343+spankaxota@users.noreply.github.com>
2024-10-11 12:22:08 -0300

    hujku

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ function criaCartao(Categoria,Pergunta,Resposta){
     let container = document.getElementsByName('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = `
-    <div class="conteudo-cartao">
-<h3>Categoria</h3>
-<div class="pergunta-cartao"></div>pergunta-cartao
-<p>pergunta</p>
+    cartao.innerHTML = `<div class="conteudo-cartao">
+    <h3>${Categoria}</h3>
+    <div class="pergunta-cartao">
+        <p>${Pergunta}</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>${Resposta}</p>
+    </div>
 </div>
-<div class="resposta-cartao">
-<p>pergunta</p>
-</div>
+
+    
     `
 
 }
